88a248b567b29bdc5b9b3d8d980ef48c7dc01bd4
Nurza <gabriel.mgt0+git@gmail.com>
2015-08-13 11:49:54 +0200

    Add feature: get docker info & version

--- core/http-server.go
+++ core/http-server.go
@@ -30,9 +30,63 @@ func HTTPHandler(w http.ResponseWriter, r *http.Request) {
 	// Check first URI item
 	switch URIItems[1] {
 	case "info":
-		fmt.Fprint(w, "/info TODO")
-	case "version":
-		fmt.Fprint(w, "/version TODO")
+		var tmpDockerInfo dapi.DockerInfo       // Docker API info
+		var tmpDockerVersion dapi.DockerVersion // Docker API version
+		var dockerInfo dguard.DockerInfo        // DGC Docker info
+		var status int                          // HTTP status returned
+		var body string                         // HTTP body returned
+		var err error                           // Error handling
+
+		// Get docker info
+		status, body = HTTPReq("/info")
+		if status != 200 {
+			l.Error("Can't get docker info, status:", status)
+		}
+
+		// Parse returned json
+		err = json.Unmarshal([]byte(body), &tmpDockerInfo)
+		if err != nil {
+			l.Error("Parsing docker info:", err)
+		}
+
+		// Get docker version
+		status, body = HTTPReq("/version")
+		if status != 200 {
+			l.Error("Can't get docker version, status:", status)
+		}
+
+		// Parse returned json
+		err = json.Unmarshal([]byte(body), &tmpDockerVersion)
+		if err != nil {
+			l.Error("Parsing docker version:", err)
+		}
+
+		// Set dockerInfo
+		dockerInfo.ID = tmpDockerInfo.ID
+		dockerInfo.Name = tmpDockerInfo.Name
+		dockerInfo.Containers = tmpDockerInfo.Containers
+		dockerInfo.Images = tmpDockerInfo.Images
+		dockerInfo.Driver = tmpDockerInfo.Driver
+		dockerInfo.SystemTime = tmpDockerInfo.SystemTime
+		dockerInfo.OperatingSystem = tmpDockerInfo.OperatingSystem
+		dockerInfo.NCPU = tmpDockerInfo.NCPU
+		dockerInfo.MemTotal = tmpDockerInfo.MemTotal
+
+		dockerInfo.APIVersion = tmpDockerVersion.APIVersion
+		dockerInfo.Arch = tmpDockerVersion.Arch
+		dockerInfo.Experimental = tmpDockerVersion.Experimental
+		dockerInfo.GitCommit = tmpDockerVersion.GitCommit
+		dockerInfo.GoVersion = tmpDockerVersion.GoVersion
+		dockerInfo.KernelVersion = tmpDockerVersion.KernelVersion
+		dockerInfo.Os = tmpDockerVersion.Os
+		dockerInfo.Version = tmpDockerVersion.Version
+
+		// dockerInfo => json
+		tmpJson, _ := json.Marshal(dockerInfo)
+
+		// Add json to the returned string
+		returnStr = string(tmpJson)
+
 	case "list": // Get container list with basic info
 		var tmpContainerList []dguard.Container // Temporary DGC Container list
 
